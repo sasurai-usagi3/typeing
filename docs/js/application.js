2a44ef9dcfacab25f9fae5a76ff2f82cf269350d
宇山拓夢 <sasurai.usagi3@gmail.com>
2016-12-17 10:30:56 +0900

    スタックにキーを保存

--- docs/js/application.js
+++ docs/js/application.js
@@ -1,11 +1,24 @@
 window.addEventListener("load", () => {
   let label = document.getElementById("label");
   let str = "hello!";
+  let keyStack = "";
 
   str.split("").forEach(x => {
     let span = document.createElement("span");
     span.textContent = x;
+    span.className = "label__font_normal";
 
     label.appendChild(span);
+  });
+
+  window.addEventListener("keydown", e => {
+    if(e.key.length !== 1 && e.key !== "Backspace") {
+      return;
+    }
+    if(e.key === "Backspace") {
+      keyStack = keyStack.slice(0, -1);
+    } else {
+      keyStack += e.key;
+    }
   });
 });
